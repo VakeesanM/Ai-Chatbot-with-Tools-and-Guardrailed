c8c5bf7aee4b3c14589c8307a3e2b62a4b346252
SAMVM1128 <vakeesan.moorthy@gmail.com>
2026-08-24 11:09:00 -0700

    added math and search tools

---
+++ src/tools/math.py
@@ -0,0 +1,33 @@
+from langchain_core.tools import tool
+
+
+@tool
+def add(num1, num2):
+    """Adds two numbers together and returns their sum"""
+    return num1 + num2
+
+@tool
+def multiply(num1, num2):
+    """Multiplies two numbers together and return their produce"""
+    return num1 * num2
+
+
+@tool
+def divide(dividend, divisor):
+    """Divides the dividend by divisor and return the quotient"""
+    return dividend / divisor
+
+@tool
+def subtract(num1, num2):
+    """Subtracts num1 from num2 and returns the answer"""
+    return num2 - num1
+
+@tool
+def power(number, power):
+    """Returns the product of inputed number to the inputed power"""
+    return number ** power
+
+@tool
+def root(number, root_power):
+    """Returns the product of inputed number to the inputed root"""
+    return number ** (1/power)
