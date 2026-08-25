9d84ec8731a90d74ccc17141dafe27be8c35039e
SAMVM1128 <vakeesan.moorthy@gmail.com>
2026-08-25 16:41:29 -0700

    created front end page

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+#from chatbot.bot import ChatBot
+
+st.set_page_config(page_title="Chatbot with Guardrails", page_icon='🤖')
+st.header("Chatbot with Guardrails - Demo!")
+st.write("This Chatbot agent has memory and guardrails. It can deal with PII info(Credit Card, IP, and Email) and guardrails that can deal with prompt injection to a minor level")
+st.write("It also has a few tools, allowing it solve math problems as well surf the web")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "AI", "content": "Hi, how can I help you today?"}]
+    #st.session_state.chatbot = ChatBot()
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content']) 
+
+
+if prompt := st.chat_input("How is it going?"):
+    with st.chat_message('user'):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    
+    with st.chat_message("AI"):
+        st.markdown(f"Echo: {prompt}")
+    st.session_state.messages.append({"role": "AI", "content": f"Echo: {prompt}"})
+
+    
+
+
